dc9689520ecd9e7465892c733cef3aaf7f0385a1
Edirisinghe-Li <s23009669@ousl.lk>
2025-05-29 18:22:56 +0530

    add avatar icon and menu

--- src/Component/LoginRegister/NavBar.jsx
+++ src/Component/LoginRegister/NavBar.jsx
@@ -4,24 +4,79 @@ import './NavBar.css';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { FaBus } from "react-icons/fa";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const NavBar = () => {
 
-    const navRef = useRef();
+        const [anchorElNav, setAnchorElNav] = React.useState(null);
+        const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
-    return(
-        <header>
-            <h3><FaBus className="icon"/>SmartMove</h3>
-            <nav ref={navRef}>
-                <Link to ="/home" className="nav-links">Home</Link>
-                <Link to ="/dashboard" className="nav-links">Dashboard</Link>
-                <Link to ="/#" className="nav-links">Contact</Link>
-                <Link to ="/login" className="nav-links">Login</Link>
+        const handleOpenNavMenu = (event) => {
+            setAnchorElNav(event.currentTarget);
+        };
+        const handleOpenUserMenu = (event) => {
+            setAnchorElUser(event.currentTarget);
+        };
+
+        const handleCloseNavMenu = () => {
+            setAnchorElNav(null);
+        };
+
+        const handleCloseUserMenu = () => {
+            setAnchorElUser(null);
+        };
+
+
+            const navRef = useRef();
+
+            const showNavbar = () => {
+                navRef.current.classList.toggle("responsive_nav");
+            }
+            return(
+                <header>
+                    <h3><FaBus className="icon"/>SmartMove</h3>
+                    <nav ref={navRef}>
+                        <Link to ="/home" className="nav-links">Home</Link>
+                        <Link to ="/dashboard" className="nav-links">Dashboard</Link>
+                        <Link to ="/#" className="nav-links">Contact</Link>
+                        <Link to ="/login" className="nav-links">Login</Link>
+                        <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    </IconButton>
+                    </Tooltip>
+                    <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                    >
+                    {settings.map((setting) => (
+                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                        <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                        </MenuItem>
+                    ))}
+                    </Menu>
+                </Box>
                 <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes/>
                 </button>
